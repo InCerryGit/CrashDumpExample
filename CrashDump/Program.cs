5f9b8958faeaa68bdfeb10bd5c6aa0172a98ce59
InCerry <incerry@foxmail.com>
2022-11-01 09:57:58 +0800

    init

---
+++ CrashDump/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+foreach (DictionaryEntry environmentVariable in Environment.GetEnvironmentVariables())
+{
+    if(environmentVariable.Key.ToString()?.StartsWith("DOTNET_") == false) continue;
+    Console.WriteLine($"{environmentVariable.Key}={environmentVariable.Value}");
+}
+
+Method();
+
+[MethodImpl(MethodImplOptions.NoInlining)]
+static void Method()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        if(i > 50)
+            throw new Exception("Crash");
+    }
+}
---
+++ CrashDump/run.bat
@@ -0,0 +1,6 @@
+@SET DOTNET_DbgEnableMiniDump=1
+@SET DOTNET_DbgMiniDumpName="dump.dmp"
+@SET DOTNET_CreateDumpDiagnostics=1
+@SET DOTNET_EnableCrashReport=1
+@SET COMPlus_EnableCrashReport=1
+@dotnet CrashDump.dll
